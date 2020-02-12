9f82987d0865ba4039a7feb7a85c4f2495061d03
ReaganLeon00 <maggie.leon00@gmail.com>
2020-02-12 14:58:30 -0700

    add code
    pg 131

---
+++ pg 131/pg 131/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Assignment_pg131
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string text = "This assignment " +
+                          "is " +
+                          " on page 131.";
+
+            Console.WriteLine(text);
+            Console.ReadLine();
+
+            string upperCase = "Uppercase string";
+            string upperCase1 = upperCase.ToUpper();
+            Console.WriteLine(upperCase1);
+            Console.ReadLine();
+
+            System.Text.StringBuilder builder = new System.Text.StringBuilder("Names");
+            builder.Append(", ");
+            builder.Append("Sarah");
+            builder.Append(", Phil");
+            builder.Insert(0, "Tara, ");
+            builder.Insert(10, 900);
+            builder.Append(", ");
+            Console.WriteLine(builder);
+
+            Console.ReadLine();
+        }
+    }
+}
