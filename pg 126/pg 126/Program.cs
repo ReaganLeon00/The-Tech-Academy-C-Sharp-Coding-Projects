2def437dfed54419b2f5cbc4b8cd1c2c9d3dda57
ReaganLeon00 <maggie.leon00@gmail.com>
2020-02-12 13:12:02 -0700

    add code
    pg 126

---
+++ pg 126/pg 126/Program.cs
@@ -0,0 +1,49 @@
+﻿using System;
+
+namespace pg_126
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Guess a number between 1-5?");
+            int number = Convert.ToInt32(Console.ReadLine());
+            bool isGuessed = number == 4;
+
+            do
+            {
+                switch (number)
+                {
+                    case 1:
+                        Console.WriteLine("You guessed 1. Try again.");
+                        Console.WriteLine("Guess a number between 1-5?");
+                        number = Convert.ToInt32(Console.ReadLine());
+                        break;
+                    case 2:
+                        Console.WriteLine("You guessed 2. Try again.");
+                        Console.WriteLine("Guess a number between 1-5?");
+                        number = Convert.ToInt32(Console.ReadLine());
+                        break;
+                    case 3:
+                        Console.WriteLine("You guessed 3. Try again.");
+                        Console.WriteLine("Guess a number between 1-5?");
+                        number = Convert.ToInt32(Console.ReadLine());
+                        break;
+                    case 4:
+                        Console.WriteLine("You guessed the number 4. That is correct!");
+                        isGuessed = true;
+                        Console.ReadLine();
+                        break;
+                    case 5:
+                        Console.WriteLine("You guessed 5. Try again.");
+                        Console.WriteLine("Guess a number between 1-5?");
+                        number = Convert.ToInt32(Console.ReadLine());
+                        break;
+
+                }
+            }
+            while (!isGuessed);
+            
+        }
+    }
+}
