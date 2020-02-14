76846f57e9c87df6b82981232ec13466abebb0c3
ReaganLeon00 <maggie.leon00@gmail.com>
2020-02-14 11:55:45 -0700

    fix code
    126

--- pg 126/pg 126/Program.cs
+++ pg 126/pg 126/Program.cs
@@ -43,37 +43,40 @@ namespace pg_126
                 }
             }
             while (!isGuessed);
-            {
-                switch (number)
-                {
-                    case 1:
-                        Console.WriteLine("You guessed 1. Try again.");
-                        Console.WriteLine("Guess a number between 1-5?");
-                        number = Convert.ToInt32(Console.ReadLine());
-                        break;
-                    case 2:
-                        Console.WriteLine("You guessed the number 2. You are correct!");
-                        Console.WriteLine("Guess a number between 1-5?");
-                        number = Convert.ToInt32(Console.ReadLine());
-                        break;
-                    case 3:
-                        Console.WriteLine("You guessed 3. Try again.");
-                        Console.WriteLine("Guess a number between 1-5?");
-                        number = Convert.ToInt32(Console.ReadLine());
-                        break;
-                    case 4:
-                        Console.WriteLine("You guessed 4. Try again.");
-                        isGuessed = true;
-                        Console.ReadLine();
-                        break;
-                    case 5:
-                        Console.WriteLine("You guessed 5. Try again.");
-                        Console.WriteLine("Guess a number between 1-5?");
-                        number = Convert.ToInt32(Console.ReadLine());
-                        break;
 
-                }
+            Console.WriteLine("Guess a number between 1-5?");
+            int number2 = Convert.ToInt32(Console.ReadLine());
+            bool isGuessed2 = number2 == 2;
+
+            while (!isGuessed2) ;
+            switch (number2)
+            {
+                case 1:
+                    Console.WriteLine("You guessed 1. Try again.");
+                    Console.WriteLine("Guess a number between 1-5?");
+                    number = Convert.ToInt32(Console.ReadLine());
+                    break;
+                case 2:
+                    Console.WriteLine("You guessed the number 2. That is correct!");
+                    isGuessed2 = true;
+                    break;
+                case 3:
+                    Console.WriteLine("You guessed 3. Try again.");
+                    Console.WriteLine("Guess a number between 1-5?");
+                    number = Convert.ToInt32(Console.ReadLine());
+                    break;
+                case 4:
+                    Console.WriteLine("You guessed 4. Try again.");
+                    number = Convert.ToInt32(Console.ReadLine());
+                    break;
+                case 5:
+                    Console.WriteLine("You guessed 5. Try again.");
+                    Console.WriteLine("Guess a number between 1-5?");
+                    number = Convert.ToInt32(Console.ReadLine());
+                    break;
+
             }
+            Console.ReadLine();
         }
     }
 }
