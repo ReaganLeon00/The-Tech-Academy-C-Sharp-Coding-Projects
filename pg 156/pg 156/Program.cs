f5769f8d5f3c3253ea6bb5771b135f5e52aa0fb1
ReaganLeon00 <maggie.leon00@gmail.com>
2020-02-24 15:59:05 -0700

    add code
    code for 156

---
+++ pg 156/pg 156/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Assignment_156
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            pg_156.threeClass a = new pg_156.threeClass();
+            Console.WriteLine("Please select a number to perform 3 basic math operations on: ");
+            int x = Convert.ToInt32(Console.ReadLine());
+
+            Console.WriteLine("Your number multiplied by 3 is equal to: " + a.multiplybyThree(x));
+            Console.WriteLine("5 subtracted from your number is equal to: " + a.subtractFive(x));
+            Console.WriteLine("Your number plus 7 is equal to: " + a.addSeven(x));
+            Console.ReadLine();
+
+
+
+        }
+    }
+}
---
+++ pg 156/pg 156/threeClass.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace pg_156
+{
+    class threeClass
+    {
+        
+            public int multiplybyThree(int x)
+            {
+                return x * 3;
+            }
+            public int subtractFive(int x)
+            {
+                return x - 5;
+
+            }
+            public int addSeven(int x)
+            {
+                return x + 7;
+            }
+
+
+        }
+    }
+
+
