bcffbba2a4979e1ff6f029c6454b3647b6a793dc
ReaganLeon00 <maggie.leon00@gmail.com>
2020-02-26 11:36:13 -0700

    add code
    pg 159

---
+++ pg 159/pg 159/Integer.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Assignment_pg159
+{
+    public class Integer
+    {
+        public int doMathMethod(int x)
+        {
+            int result = x / 15;
+            return result;
+
+        }
+        public int doMathMethod(decimal y)
+        {
+            Console.WriteLine("Please type in a decimal number: ");
+
+
+            int result2 = Convert.ToInt32(y + 8);
+            return result2;
+
+        }
+        public int doMathMethod(string z)
+        {
+            int result3 = Convert.ToInt32(z) + 10;
+            return result3;
+        }
+        
+    }
+}
+
+
+
---
+++ pg 159/pg 159/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Assignment_pg159
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Integer a = new Integer();
+
+            Console.WriteLine("Please type in a number to divide by 15: ");
+            int x = Convert.ToInt32(Console.ReadLine());
+            int result1 = a.doMathMethod(x);
+            Console.WriteLine("Your number divided by 15 is: " + result1);
+            Console.ReadLine();
+
+
+            Console.WriteLine("Please type in a decimal to add to 8: ");
+            decimal y = Convert.ToDecimal(Console.ReadLine());
+            int result2 = a.doMathMethod(y);
+            Console.WriteLine("Your decimal number added to 8 is: " + result2);
+            Console.ReadLine();
+
+            Console.WriteLine("12 + 10 is equal to:");
+            int z = a.doMathMethod("12");
+            Console.WriteLine(z);
+            Console.ReadLine();
+
+        }
+    }
+}
