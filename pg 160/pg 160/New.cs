c1a376385fdab80730e4889f0a66950dca4a15ae
ReaganLeon00 <maggie.leon00@gmail.com>
2020-02-26 13:43:45 -0700

    add code
    pg 160

---
+++ pg 160/pg 160/New.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Assignment_pg160
+{
+    class newClass
+    {
+        public int twoMethod(int x, int y = 0)
+        {
+            int result = x + y + 5;
+            return result;
+
+        }
+
+    }
+}
+
---
+++ pg 160/pg 160/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Assignment_pg160
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            newClass a = new newClass();
+
+            Console.WriteLine("Please choose a number. You can type a second number but it is not required.");
+            int x = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Second number(optional): ");
+            string y = Console.ReadLine();
+            if (y == "")
+            {
+                int result = a.twoMethod(x);
+                Console.WriteLine(result);
+                Console.ReadLine();
+
+            }
+            else
+            {
+                int yInt = Convert.ToInt32(y);
+                int result = a.twoMethod(x, yInt);
+                Console.WriteLine(result);
+                Console.ReadLine();
+
+            }
+
+
+
+
+        }
+    }
+}
