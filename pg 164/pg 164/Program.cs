695a670f8baac2569f69a1fc934763e324d86544
ReaganLeon00 <maggie.leon00@gmail.com>
2020-02-26 16:23:13 -0700

    164

---
+++ pg 164/pg 164/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace pg_164
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            voidClass a = new voidClass();
+            Console.WriteLine("Please slect a number to divide by 2: ");
+            int x = Convert.ToInt32(Console.ReadLine());
+            a.voidMethod(x);
+            Console.WriteLine("Your number divided by 2: " + a.voidMethod(x));
+            Console.ReadLine();
+
+        }
+    }
+}
---
+++ pg 164/pg 164/voidClass.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace pg_164
+{
+    class voidClass
+    {
+        public void voidMethod(int x)
+        {
+            int num = x / 2;
+        }
+    }
+}
