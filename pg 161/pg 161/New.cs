463c8cdb538cc84f0068d55a015df6fddabb2257
ReaganLeon00 <maggie.leon00@gmail.com>
2020-02-26 13:50:45 -0700

    add code
    pg 161

---
+++ pg 161/pg 161/New.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Assignment_pg161
+{
+    class newClass
+    {
+        public void addMethod(int x, int y)
+        {
+            int result = Convert.ToInt32(x + 5);
+            Console.WriteLine(y);
+            return;
+
+        }
+
+
+    }
+}
---
+++ pg 161/pg 161/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Assignment_pg161
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            newClass a = new newClass();
+
+            int x = 18;
+            int y = 7;
+            a.addMethod(x: x, y: y);
+            Console.ReadLine();
+
+        }
+    }
+}
