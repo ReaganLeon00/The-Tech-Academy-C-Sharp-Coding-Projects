ad23c1ad242a58d9ea99429855041a26f9db4d33
ReaganLeon00 <maggie.leon00@gmail.com>
2020-02-27 13:26:37 -0700

    Add code
    pg 179

---
+++ pg 179/pg 179/Employee.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace pg_179
+{
+    public class Employee : pg_179.IQuittable
+
+    {
+        public void Quit()
+        {
+            Console.Write("I quit");
+        }
+    }
+}
---
+++ pg 179/pg 179/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace pg_179
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            IQuittable employee= new Employee();
+            employee.Quit();
+            Console.ReadLine();
+        }
+    }
+}
