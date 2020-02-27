b12379f04a1fd11fafde4415be7d846d4295ee5d
ReaganLeon00 <maggie.leon00@gmail.com>
2020-02-27 15:06:26 -0700

    add code
    pg 184

---
+++ pg 184/pg 184/Employee.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace pg_184
+{
+    class Employee : Person
+    {
+        public int Id { get; set; }
+
+        public static bool operator== (Employee employee, Employee employee2)
+        {
+            if (employee.Id == employee2.Id)
+            {
+                return true;
+            }
+            else
+            {
+                return false;
+            }
+                
+        }
+        public static bool operator !=(Employee employee, Employee employee2)
+        {
+            return employee.Id != employee2.Id;
+        }
+        public override bool Equals(object obj)
+        {
+            var emp = obj as Employee;
+            if (emp == null)
+                return false;
+
+            return this.Id.Equals(emp.Id);
+        }
+
+
+    }
+}
---
+++ pg 184/pg 184/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace pg_184
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Employee employee = new Employee();
+            employee.FirstName = "Sarah";
+            employee.LastName = "Fall";
+            employee.Id = 24;
+
+            Employee employee2 = new Employee();
+            employee2.FirstName = "Chris";
+            employee2.LastName = "Angel";
+            employee.Id = 34;
+
+            Console.ReadLine();
+        }
+    }
+}
