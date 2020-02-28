7927b32bbbf06bc9b49310ac6a4c64d1d846c76f
ReaganLeon00 <maggie.leon00@gmail.com>
2020-02-28 12:15:17 -0700

    add code
    pg 192

---
+++ pg 192/pg 192/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace pg_192
+{
+    class Program
+    {
+        public enum DaysOfTheWeek
+        {
+            Monday,
+            Tuesday,
+            Wednesday,
+            Thursday,
+            Friday,
+            Saturday,
+            Sunday,
+        }
+        static void Main(string[] args)
+        {
+            try
+            {
+                Console.WriteLine("Enter a day of the week.");
+                string input = Console.ReadLine();
+                DaysOfTheWeek day = (DaysOfTheWeek)Enum.Parse(typeof(DaysOfTheWeek), input);
+                Console.WriteLine("Thank you for entering a valid day of the week!");
+                Console.ReadLine();
+            }
+            catch(ArgumentException)
+            {
+                Console.WriteLine("Please enter an actual day of the week");
+                Console.ReadLine();
+            }
+        }
+        
+
+    }
+}
