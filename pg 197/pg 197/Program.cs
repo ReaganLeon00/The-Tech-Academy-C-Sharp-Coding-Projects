c4c01116d9444f1e0c0159c7f5b59f66c3f6cd42
ReaganLeon00 <maggie.leon00@gmail.com>
2020-02-28 12:16:53 -0700

    add code
    pg 197

---
+++ pg 197/pg 197/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace pg_197
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Number num = new Number();
+            num.Amount = 5;
+            Console.WriteLine(num.Amount);
+            Console.ReadLine();
+
+        }
+    struct Number
+        {
+            public decimal Amount { get; set; }
+        }
+    }
+}
