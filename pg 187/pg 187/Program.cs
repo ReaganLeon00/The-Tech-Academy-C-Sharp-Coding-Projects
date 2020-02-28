8bceccd3cd1c35b73753cfac3a46aabfed80f059
ReaganLeon00 <maggie.leon00@gmail.com>
2020-02-27 18:09:32 -0700

    add code
    pg 187

---
+++ pg 187/pg 187/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace pg_187
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Employee<string> things = new Employee<string>(){ Things = new List<string> {"Effy","Freddie", "Cook", "Katie" } };
+            Employee<int> things2 = new Employee<int>() { Things = new List<int> { 1, 2, 3, 4 } };
+            
+            foreach (string var in things.Things)
+            {
+                Console.WriteLine(var);
+            }
+
+            foreach (int items in things2.Things)
+            {
+                Console.WriteLine(items);
+            }
+            Console.ReadLine();
+        }
+    }
+}
