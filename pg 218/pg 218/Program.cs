9fc680360075629b0e1b795d95d5555d0c843c0e
ReaganLeon00 <maggie.leon00@gmail.com>
2020-03-02 18:44:58 -0700

    add code
    pg 218

---
+++ pg 218/pg 218/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+
+namespace pg_218
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Type a number to add to a file.");
+            string text = Console.ReadLine();
+            File.WriteAllText(@"C:\Users\maggi\source\repos\pg 218\logs.txt",text);
+
+            string text2 = File.ReadAllText(@"C:\Users\maggi\source\repos\pg 218\logs.txt");
+            Console.WriteLine("The file reads " + text2);
+            Console.ReadLine();
+            
+        }
+    }
+}
