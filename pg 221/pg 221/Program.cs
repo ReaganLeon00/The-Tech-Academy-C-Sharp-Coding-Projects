3e24b88dfb59381c5a3496207581758de0e507fd
ReaganLeon00 <maggie.leon00@gmail.com>
2020-03-03 14:45:16 -0700

    add code
    pg 221

---
+++ pg 221/pg 221/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace pg_221
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            DateTime now = DateTime.Now;
+            Console.WriteLine("Current Date and Time is : " + now);
+            Console.WriteLine("Please enter a number:");
+            int input = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("It will be " + now.AddHours(input) + " in " + input + " hours ");
+            Console.ReadLine();
+
+        }
+    }
+}
