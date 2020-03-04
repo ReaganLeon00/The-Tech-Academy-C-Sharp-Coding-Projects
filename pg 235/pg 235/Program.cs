327a99ee18c39646d2b2689b020e13f7cd54158c
ReaganLeon00 <maggie.leon00@gmail.com>
2020-03-04 13:24:09 -0700

    add code
    pg 235

---
+++ pg 235/pg 235/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace pg_235
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            DateTime nowDate = DateTime.Now;
+            Console.WriteLine("What age are you?");
+            while(true)
+                {
+                try
+                {
+                    int age = Convert.ToInt32(Console.ReadLine());
+                    if (age <= 0)
+                    {
+                        Console.WriteLine("You are " + nowDate.AddYears(-age).Year + " years old.");
+                        Console.ReadLine();
+
+                    }
+                }
+
+                catch (FormatException)
+                {
+                    Console.WriteLine("Please enter a number that is not a zero or a negative!");
+                    Console.ReadLine();
+                }
+                break;
+            }
+        }
+    }
+}
