a31f39c3e1f51ff176bcb84e6d39667a4d85dc55
ReaganLeon00 <maggie.leon00@gmail.com>
2020-03-03 18:08:05 -0700

    add code
    pg 231

---
+++ pg 231/pg 231/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace pg_231
+{
+    class Program
+    {
+        private string hello;
+
+        static void Main(string[] args)
+        {
+            const string hello = "Hello";
+            string var = "Hey";
+           
+        }
+        public Program(string hello, string var) : this(hello)
+        {
+  
+            var = hello;
+        }
+
+        public Program(string hello)
+        {
+            this.hello = hello;
+        }
+        
+
+    }
+  
+}
