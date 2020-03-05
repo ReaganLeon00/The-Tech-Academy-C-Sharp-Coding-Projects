2763afc2e63a3095ace216c3821712d9bada2ac1
ReaganLeon00 <maggie.leon00@gmail.com>
2020-03-05 16:59:57 -0700

    add code
    Car Insurance

---
+++ CarInsurance/CarInsurance/Models/Customer.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace CarInsurance.Models
+{
+    public class Customer
+    {
+        public int Id { get; set; }
+        public string firstName { get; set; }
+        public string lastName { get; set; }
+        public string email { get; set; }
+        public DateTime dateOfBirth { get; set; }
+        public int carYear { get; set; }
+        public string carMake { get; set; }
+        public string carModel { get; set; }
+        public string duiCheck { get; set; }
+        public int speedingTickets { get; set; }
+        public string coverageType { get; set; }
+        public int Total { get; set; }
+    }
+}
